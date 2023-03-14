d80bfd1eb8eb8db4ca8513efc32101eb8faf0efa
Juan Benavides <Juan90270@Outlook.com>
2023-03-13 23:45:36 -0700

    Fizz Buzz complete

--- Fizz Buzz.py
+++ Fizz Buzz.py
@@ -1,2 +1,12 @@
 
 fizz_buzz = range(1, 51)
+
+for num in fizz_buzz:
+    if num % 15 == 0:
+        print("Fizz Buzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+        print(num)
